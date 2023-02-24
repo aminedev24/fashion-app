99341c0d15e7bf4f0a392a824e8c9ec9afa4f2e9
aminedz24 <abdoufuntuber@gmail.com>
2023-02-24 21:45:55 +0100

    Updates

---
+++ precache-manifest.92457b372229c9e002f27586fa38ff81.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "6c77231592952e6cc4cc",
+    "url": "/fashion-app/static/css/main.8bbe6496.chunk.css"
+  },
+  {
+    "revision": "6c77231592952e6cc4cc",
+    "url": "/fashion-app/static/js/main.6c772315.chunk.js"
+  },
+  {
+    "revision": "444f0bc38a785630c6eb",
+    "url": "/fashion-app/static/js/1.444f0bc3.chunk.js"
+  },
+  {
+    "revision": "666fef0f6e7ee81a4b1b",
+    "url": "/fashion-app/static/js/2.666fef0f.chunk.js"
+  },
+  {
+    "revision": "9d394299aba64c517432",
+    "url": "/fashion-app/static/js/runtime~main.9d394299.js"
+  },
+  {
+    "revision": "3f5bae22a090bd7e08f4e104f1177217",
+    "url": "/fashion-app/index.html"
+  }
+];
